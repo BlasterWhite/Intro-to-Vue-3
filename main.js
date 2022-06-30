20202f65cd7cca490aa398478b4c421b32327de7
Matéo G <icarambala@gmail.com>
2022-06-30 22:58:00 +0200

    Intro Vue 3.2

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data: function() {
+        return {
+            product: 'Socks',
+            desc: 'A pair of warm socks',
+        }
+    }
+})
